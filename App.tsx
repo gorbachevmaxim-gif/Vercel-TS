3b070e86039d08bc1e3359d8b64d7342139f26a7
max-gorbie <gorbachev.maxim@gmail.com>
2026-01-08 15:56:43 +0300

    header

--- App.tsx
+++ App.tsx
@@ -96,11 +96,11 @@ const App: React.FC = () => {
       <div className="bg-white border-b border-slate-200 sticky top-0 z-10 px-4 py-3 shadow-sm">
         <h1 
             onClick={() => setSelectedCity(null)}
-            className="text-xl font-bold cursor-pointer text-center text-[rgb(64,72,35)]"
+            className="text-lg font-bold cursor-pointer text-center text-[rgb(64,72,35)]"
         >
             ПОДБОР МЕСТА ДЛЯ РАЙДА
         </h1>
-        <p className="text-xs text-center text-[#404823]">Поиск идеальной погоды без осадков (09:00 - 18:00)</p>
+        <p className="text-xs text-center text-[#404823]">поиск идеальной погоды без осадков</p>
       </div>
 
       <div className="max-w-2xl mx-auto p-4 space-y-6">
