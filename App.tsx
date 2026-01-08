df18a76874829c1934f98825ca5537f3e95f1673
max-gorbie <gorbachev.maxim@gmail.com>
2026-01-08 19:03:17 +0300

    maps

--- App.tsx
+++ App.tsx
@@ -100,7 +100,7 @@ const App: React.FC = () => {
         >
             ПОДБОР МЕСТА ДЛЯ РАЙДА
         </h1>
-        <p className="text-xs text-center text-[#404823]">поиск идеальной погоды без осадков</p>
+        <p className="text-xs text-center text-[#404823]">поиск идеальной погоды без осадков на сб и вс</p>
       </div>
 
       <div className="max-w-2xl mx-auto p-4 space-y-6">
