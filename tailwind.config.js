3643eeda12c0515025bc8b4b48be82615dfa9889
max-gorbie <gorbachev.maxim@gmail.com>
2026-01-09 23:29:01 +0300

    design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     "./services/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        DEFAULT: "#edebe5"
+      }
+    },
   },
   plugins: [],
 }
