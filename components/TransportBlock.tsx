5c86994e1bf56565c193e1e4b44743bae5d9b602
max-gorbie <gorbachev.maxim@gmail.com>
2026-01-09 19:04:54 +0300

    design

--- components/TransportBlock.tsx
+++ components/TransportBlock.tsx
@@ -200,17 +200,7 @@ const TransportBlock: React.FC<TransportBlockProps> = ({
                         <span className="text-xs font-medium uppercase mb-1" style={{ color: '#404823' }}>Туда</span>
                         <span className="font-bold text-slate-800 group-hover:text-[#4f6814] transition-colors">Москва → {startConfig.displayName}</span>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-slate-300 group-hover:text-[#4f6814]">
-                        {isAeroflot ? (
-                            <path d="M22 2L11 13M22 2l-7 20-4-9-9-4 20-7z" /> 
-                        ) : (
-                            <>
-                                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                <polyline points="15 3 21 3 21 9"></polyline>
-                                <line x1="10" y1="14" x2="21" y2="3"></line>
-                            </>
-                        )}
-                    </svg>
+                    <svg width="24" height="24" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.848.807a12 12 0 1 1-12 12 12 12 0 0 1 12-12Z" fill="#FC0"></path><path d="m14.998 18.275-1.326 1.17-2.99 2.834h4.902l5.746-5.744-5.728-5.728H10.7l.056.052 1.256 1.11 2.992 2.838H1.018c.203 1.208.59 2.378 1.15 3.468h12.83Z" fill="url(#serviceLogo)"></path><g filter="url(#serviceLogoFilter)"><path d="M10.1 14.807h4.904l-3-2.834-1.256-1.11-.07-.06h14a11.98 11.98 0 0 0-1.152-3.47H10.7l1.322-1.19 2.964-2.8h-4.902L4.356 9.07l5.744 5.738Z" fill="#fff"></path></g><defs><linearGradient id="serviceLogo" x1="11.174" y1="22.279" x2="11.174" y2="10.807" gradientUnits="userSpaceOnUse"><stop stop-color="#FC3F1D"></stop><stop offset="1" stop-color="#FF2700"></stop></linearGradient><filter id="serviceLogoFilter" x="3.727" y="2.923" width="21.162" height="12.304" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset dx="-.21"></feOffset><feGaussianBlur stdDeviation=".21"></feGaussianBlur><feColorMatrix values="0 0 0 0 0.0509804 0 0 0 0 0.137255 0 0 0 0 0.262745 0 0 0 0.05 0"></feColorMatrix><feBlend in2="BackgroundImageFix" result="effect1_dropShadow_15_8407"></feBlend><feBlend in="SourceGraphic" in2="effect1_dropShadow_15_8407" result="shape"></feBlend></filter></defs></svg>
                 </a>
             )}
 
@@ -225,17 +215,7 @@ const TransportBlock: React.FC<TransportBlockProps> = ({
                         <span className="text-xs font-medium uppercase mb-1" style={{ color: '#404823' }}>Обратно</span>
                         <span className="font-bold text-slate-800 group-hover:text-[#4f6814] transition-colors">{endConfig.displayName} → Москва</span>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-slate-300 group-hover:text-[#4f6814]">
-                        {isAeroflot ? (
-                             <path d="M22 2L11 13M22 2l-7 20-4-9-9-4 20-7z" transform="rotate(180 12 12)" />
-                        ) : (
-                            <>
-                                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                                <polyline points="15 3 21 3 21 9"></polyline>
-                                <line x1="10" y1="14" x2="21" y2="3"></line>
-                            </>
-                        )}
-                    </svg>
+                    <svg width="24" height="24" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12.848.807a12 12 0 1 1-12 12 12 12 0 0 1 12-12Z" fill="#FC0"></path><path d="m14.998 18.275-1.326 1.17-2.99 2.834h4.902l5.746-5.744-5.728-5.728H10.7l.056.052 1.256 1.11 2.992 2.838H1.018c.203 1.208.59 2.378 1.15 3.468h12.83Z" fill="url(#serviceLogo)"></path><g filter="url(#serviceLogoFilter)"><path d="M10.1 14.807h4.904l-3-2.834-1.256-1.11-.07-.06h14a11.98 11.98 0 0 0-1.152-3.47H10.7l1.322-1.19 2.964-2.8h-4.902L4.356 9.07l5.744 5.738Z" fill="#fff"></path></g><defs><linearGradient id="serviceLogo" x1="11.174" y1="22.279" x2="11.174" y2="10.807" gradientUnits="userSpaceOnUse"><stop stop-color="#FC3F1D"></stop><stop offset="1" stop-color="#FF2700"></stop></linearGradient><filter id="serviceLogoFilter" x="3.727" y="2.923" width="21.162" height="12.304" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"></feColorMatrix><feOffset dx="-.21"></feOffset><feGaussianBlur stdDeviation=".21"></feGaussianBlur><feColorMatrix values="0 0 0 0 0.0509804 0 0 0 0 0.137255 0 0 0 0 0.262745 0 0 0 0.05 0"></feColorMatrix><feBlend in2="BackgroundImageFix" result="effect1_dropShadow_15_8407"></feBlend><feBlend in="SourceGraphic" in2="effect1_dropShadow_15_8407" result="shape"></feBlend></filter></defs></svg>
                 </a>
             )}
         </div>
